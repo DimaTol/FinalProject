6b4af66a888206810277b39544c20a226c432dfe
DimaTol <d.tolkachenko@ya.ru>
2022-12-01 15:39:00 +0300

    Задаем размерность нового массива

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -6,3 +6,19 @@
 
 string[] Array1 = new string[8] { "hello", "2", "world", ":-)", "-2", "yes", "strong", "ok" };
 
+string[] ArrayLength(string[] Array1)
+{
+
+    int count = default;
+    for (int i = 0; i < Array1.Length; i++)
+    {
+        if (Array1[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] Array2 = new string[count];
+
+    return Array2;
+}
